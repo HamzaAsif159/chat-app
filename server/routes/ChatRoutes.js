01b44095af5d001ebf65714224fe47e7299d19b1
HamzaAsif159 <hmziiiraj@gmail.com>
2026-01-23 18:29:56 +0500

    feat: add chat

---
+++ server/routes/ChatRoutes.js
@@ -0,0 +1,12 @@
+// backend/routes/chat.js
+import express from "express";
+import { sendMessage, getChats, createChat } from "../controllers/chatController.js";
+import { verifyToken } from "../middlewares/AuthMiddleware.js";
+
+const router = express.Router();
+
+router.post("/send", verifyToken, sendMessage);
+router.get("/chats", verifyToken, getChats);
+router.post("/create", verifyToken, createChat);
+
+export default router;
